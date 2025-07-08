67870f03f5f1bc96f940fbda103bc4e3f1aa19ae
Kheims <djamelmesbah141@gmail.com>
2025-07-08 17:54:59 +0200

    Add Multiple file handling

--- src/main/com/kitcode/ASTGenerator.java
+++ src/main/com/kitcode/ASTGenerator.java
@@ -4,12 +4,15 @@ import antlr.Java8Lexer;
 import antlr.Java8Parser;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import java.util.ArrayList;
 
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -19,30 +22,28 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 public class ASTGenerator {
 
-    static ArrayList<String> LineNum = new ArrayList<String>();
-    static ArrayList<String> Type = new ArrayList<String>();
-    static ArrayList<String> Content = new ArrayList<String>();
+    static ArrayList<String> LineNum;
+    static ArrayList<String> Type;
+    static ArrayList<String> Content;
 
-    private static String readFile() throws IOException {
-        File file = new File("resource/java/Blabla.java");
+    private static String readFile(String fileName) throws IOException {
+        File file = new File(fileName);
         byte[] encoded = Files.readAllBytes(file.toPath());
         return new String(encoded, Charset.forName("UTF-8"));
     }
 
     public static void main(String args[]) throws IOException{
-        String inputString = readFile();
-        ANTLRInputStream input = new ANTLRInputStream(inputString);
-        Java8Lexer lexer = new Java8Lexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        Java8Parser parser = new Java8Parser(tokens);
-        ParserRuleContext ctx = parser.compilationUnit();
-
-        generateAST(ctx, false, 0);
-
-	System.out.println("digraph G {");
-        printDOT();
-	System.out.println("}");
-
+        File javaDir = new File("resource/java");
+        File[] javaFiles = javaDir.listFiles((dir, name) -> name.endsWith(".java"));
+        
+        if (javaFiles == null || javaFiles.length == 0) {
+            System.out.println("No Java files found in resource/java directory");
+            return;
+        }
+        
+        for (File javaFile : javaFiles) {
+            processJavaFile(javaFile);
+        }
     }
 
     private static void generateAST(RuleContext ctx, boolean verbose, int indentation) {
@@ -62,18 +63,44 @@ public class ASTGenerator {
         }
     }
     
-    private static void printDOT(){
-        printLabel();
+    private static void processJavaFile(File javaFile) throws IOException {
+        LineNum = new ArrayList<String>();
+        Type = new ArrayList<String>();
+        Content = new ArrayList<String>();
+        
+        String inputString = readFile(javaFile.getPath());
+        ANTLRInputStream input = new ANTLRInputStream(inputString);
+        Java8Lexer lexer = new Java8Lexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Java8Parser parser = new Java8Parser(tokens);
+        ParserRuleContext ctx = parser.compilationUnit();
+
+        generateAST(ctx, false, 0);
+
+        String fileName = javaFile.getName().replace(".java", ".dot");
+        String outputPath = "resource/java/" + fileName;
+        
+        try (FileWriter writer = new FileWriter(outputPath)) {
+            writer.write("digraph G {\n");
+            writeDOT(writer);
+            writer.write("}\n");
+        }
+        
+        System.out.println("Generated AST for " + javaFile.getName() + " -> " + fileName);
+    }
+    
+    private static void writeDOT(FileWriter writer) throws IOException {
+        writeLabel(writer);
         int pos = 0;
         for(int i=1; i<LineNum.size();i++){
             pos=getPos(Integer.parseInt(LineNum.get(i))-1, i);
-            System.out.println((Integer.parseInt(LineNum.get(i))-1)+Integer.toString(pos)+"->"+LineNum.get(i)+i);
+            writer.write((Integer.parseInt(LineNum.get(i))-1)+Integer.toString(pos)+"->"+LineNum.get(i)+i+"\n");
         }
     }
     
-    private static void printLabel(){
+    private static void writeLabel(FileWriter writer) throws IOException {
         for(int i =0; i<LineNum.size(); i++){
-            System.out.println(LineNum.get(i)+i+"[label=\""+Type.get(i)+"\\n "+Content.get(i)+" \"]");
+            writer.write(LineNum.get(i)+i+"[label=\""+Type.get(i)+"\\n "+Content.get(i)+" \"]\n");
         }
     }
     

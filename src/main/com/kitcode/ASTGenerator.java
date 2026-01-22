1f7da8ca60f8f4d9d4fc09062db20152c9f91cc8
Kheims <djamelmesbah141@gmail.com>
2026-01-22 13:32:22 +0100

    Enhance AST generation with line number tracking and improved error handling

--- src/main/com/kitcode/ASTGenerator.java
+++ src/main/com/kitcode/ASTGenerator.java
@@ -28,6 +28,9 @@ public class ASTGenerator {
     static ArrayList<String> LineNum;
     static ArrayList<String> Type;
     static ArrayList<String> Content;
+    static ArrayList<String> TypeName;
+    static ArrayList<String> StartLine;
+    static ArrayList<String> EndLine;
 
     private static String readFile(String fileName) throws IOException {
         File file = new File(fileName);
@@ -54,10 +57,24 @@ public class ASTGenerator {
 
         if (!toBeIgnored) {
             String ruleName = Java8Parser.ruleNames[ctx.getRuleIndex()];
-	    LineNum.add(Integer.toString(indentation));
-            Type.add(ruleName);
+            int ruleIndex = ctx.getRuleIndex();
+            
+            // Get line numbers from the context
+            int startLine = 1;
+            int endLine = 1;
+            if (ctx instanceof ParserRuleContext) {
+                ParserRuleContext parserCtx = (ParserRuleContext) ctx;
+                startLine = parserCtx.getStart() != null ? parserCtx.getStart().getLine() : 1;
+                endLine = parserCtx.getStop() != null ? parserCtx.getStop().getLine() : startLine;
+            }
+            
+            LineNum.add(Integer.toString(indentation));
+            Type.add(Integer.toString(ruleIndex));
+            TypeName.add(ruleName);
             Content.add(ctx.getText());
-	}
+            StartLine.add(Integer.toString(startLine));
+            EndLine.add(Integer.toString(endLine));
+        }
         for (int i = 0; i < ctx.getChildCount(); i++) {
             ParseTree element = ctx.getChild(i);
             if (element instanceof RuleContext) {
@@ -70,6 +87,9 @@ public class ASTGenerator {
         LineNum = new ArrayList<String>();
         Type = new ArrayList<String>();
         Content = new ArrayList<String>();
+        TypeName = new ArrayList<String>();
+        StartLine = new ArrayList<String>();
+        EndLine = new ArrayList<String>();
         
         String inputString = readFile(javaFile.getPath());
         ANTLRInputStream input = new ANTLRInputStream(inputString);
@@ -106,40 +126,117 @@ public class ASTGenerator {
         
         try (FileWriter writer = new FileWriter(outputPath)) {
             writer.write("digraph G {\n");
-            writeDOT(writer);
+            writeDOT(writer, null);
             writer.write("}\n");
         }
         
         System.out.println("Generated AST for " + javaFile.getName() + " -> " + fileName);
     }
+
+    public static boolean generateDotFromString(String inputString, String outputPath, String header) throws IOException {
+        LineNum = new ArrayList<String>();
+        Type = new ArrayList<String>();
+        Content = new ArrayList<String>();
+        TypeName = new ArrayList<String>();
+        StartLine = new ArrayList<String>();
+        EndLine = new ArrayList<String>();
+
+        ANTLRInputStream input = new ANTLRInputStream(inputString);
+        Java8Lexer lexer = new Java8Lexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Java8Parser parser = new Java8Parser(tokens);
+
+        parser.removeErrorListeners();
+        parser.addErrorListener(new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                System.err.println("Parsing error at line " + line + ":" + charPositionInLine + " - " + msg);
+            }
+        });
+
+        ParserRuleContext ctx = parser.compilationUnit();
+        if (parser.getNumberOfSyntaxErrors() > 0) {
+            return false;
+        }
+
+        generateAST(ctx, false, 0);
+
+        try (FileWriter writer = new FileWriter(outputPath)) {
+            writer.write("digraph G {\n");
+            writeDOT(writer, header);
+            writer.write("}\n");
+        }
+
+        return true;
+    }
+
+    public static boolean parseOnly(String inputString, String sourceName) {
+        ANTLRInputStream input = new ANTLRInputStream(inputString);
+        Java8Lexer lexer = new Java8Lexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Java8Parser parser = new Java8Parser(tokens);
+
+        parser.removeErrorListeners();
+        parser.addErrorListener(new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                if (sourceName != null && !sourceName.isEmpty()) {
+                    System.err.println("Parsing error in " + sourceName + " at line " + line + ":" + charPositionInLine + " - " + msg);
+                } else {
+                    System.err.println("Parsing error at line " + line + ":" + charPositionInLine + " - " + msg);
+                }
+            }
+        });
+
+        parser.compilationUnit();
+        return parser.getNumberOfSyntaxErrors() == 0;
+    }
     
-    private static void writeDOT(FileWriter writer) throws IOException {
+    private static void writeDOT(FileWriter writer, String header) throws IOException {
+        if (header != null && !header.isEmpty()) {
+            writer.write("graph [comment=\"" + header + "\"]\n");
+        }
+        // Store original indentation levels before writeLabel modifies LineNum
+        ArrayList<String> originalIndentations = new ArrayList<String>(LineNum);
+        
         writeLabel(writer);
-        int pos = 0;
-        for(int i=1; i<LineNum.size();i++){
-            pos=getPos(Integer.parseInt(LineNum.get(i))-1, i);
-            writer.write((Integer.parseInt(LineNum.get(i))-1)+Integer.toString(pos)+"->"+LineNum.get(i)+i+"\n");
+        
+        // Generate edges based on the tree structure
+        for(int i = 1; i < LineNum.size(); i++) {
+            int currentLevel = Integer.parseInt(originalIndentations.get(i));
+            
+            // Find parent (previous node with level currentLevel-1)
+            for(int j = i - 1; j >= 0; j--) {
+                int parentLevel = Integer.parseInt(originalIndentations.get(j));
+                if(parentLevel == currentLevel - 1) {
+                    writer.write("\"" + LineNum.get(j) + "\" -> \"" + LineNum.get(i) + "\"\n");
+                    break;
+                }
+            }
         }
     }
     
     private static void writeLabel(FileWriter writer) throws IOException {
-        for(int i =0; i<LineNum.size(); i++){
+        for(int i = 0; i < LineNum.size(); i++) {
             String escapedContent = Content.get(i).replace("\\", "\\\\")
                                                   .replace("\"", "\\\"")
                                                   .replace("\n", "\\n")
                                                   .replace("\r", "\\r")
                                                   .replace("\t", "\\t");
-            writer.write(LineNum.get(i)+i+"[label=\""+Type.get(i)+"\\n "+escapedContent+" \"]\n");
+            
+            // Generate a unique node ID using object hash-like approach
+            String nodeId = Integer.toString(Math.abs((escapedContent + i).hashCode()));
+            
+            writer.write("\"" + nodeId + "\" [ label=\"" + escapedContent + "\" ");
+            writer.write("type=" + Type.get(i) + " ");
+            writer.write("typeName=" + TypeName.get(i) + " ");
+            writer.write("startLineNumber=" + StartLine.get(i) + " ");
+            writer.write("endLineNumber=" + EndLine.get(i) + " ");
+            writer.write("]\n");
+            
+            // Store the nodeId for edge generation
+            LineNum.set(i, nodeId);
         }
     }
     
-    private static int getPos(int n, int limit){
-        int pos = 0;
-        for(int i=0; i<limit;i++){
-            if(Integer.parseInt(LineNum.get(i))==n){
-                pos = i;
-            }
-        }
-        return pos;
-    }
 }
